3a8f7d80b98fade3cb7667d457880e0b4917ee6d
Denitsa <denitsa.dey@gmail.com>
2022-03-16 10:35:00 +0300

    Implemented initial tree diagram from gojs library

---
+++ Story Tree/story-tree/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DiagramComponent } from './diagram/diagram.component';
+import { InspectorComponent } from './inspector/inspector.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiagramComponent,
+    InspectorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Story Tree/story-tree/src/app/inspector/inspector.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit } from '@angular/core';
+import * as go from 'gojs';
+
+@Component({
+  selector: 'stapp-inspector',
+  templateUrl: './inspector.component.html',
+  styleUrls: ['./inspector.component.css']
+})
+export class InspectorComponent{
+
+  public _selectedNode: go.Node;
+  public data = {
+    name: null,
+    parent: null
+  };
+
+  /*we need to make sure that when we use the Inspector
+   to edit a Node's name property, that change affects the Model defined in the app component.ts
+   Before we can do that, we need to let the inspector component know which Node (if any)
+   is selected in the Diagaram. To do that we must define a new Angular @Output() property in the diagram component,
+   that will be called nodeClicked and will be a new Angular eventEmmitter instance
+  */
+  @Input()
+  public model: go.Model;
+
+  @Input()
+  get selectedNode() {return this._selectedNode; }
+  set selectedNode(node: go.Node) {
+    if (node && node != null) {
+      this._selectedNode = node;
+      this.data.name = this._selectedNode.data.name;
+      this.data.parent = this._selectedNode.data.parent;
+    } else {
+      this._selectedNode = null;
+    }
+  }
+
+  constructor() { }
+
+  public onCommitForm() {
+    this.model.startTransaction();
+    this.model.set(this.selectedNode.data, 'name', this.data.name);
+    this.model.set(this.selectedNode.data, 'parent', this.data.parent);
+    this.model.commitTransaction();
+  }
+
+}
