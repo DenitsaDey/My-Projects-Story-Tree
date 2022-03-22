f7d06596d57cc26fec14e4df3dd358dec3e9ba93
Denitsa <denitsa.dey@gmail.com>
2022-03-22 10:56:32 +0300

    added register and sign in components

--- Story Tree/story-tree/src/app/feature/pages/pages.module.ts
+++ Story Tree/story-tree/src/app/feature/pages/pages.module.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
