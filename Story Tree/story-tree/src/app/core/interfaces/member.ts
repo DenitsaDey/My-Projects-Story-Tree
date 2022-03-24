16ae3f6aabb7889b12eb8b05f4b9230b7cbd0b8c
Denitsa <denitsa.dey@gmail.com>
2022-03-24 12:04:03 +0300

    Family service was created

---
+++ Story Tree/story-tree/src/app/core/interfaces/member.ts
@@ -0,0 +1,8 @@
+export interface IMember{
+    id:string;
+    name: string;
+    birthday: Date;
+    relation: string;
+    partnerId: string;
+    parents: string[];
+}
--- Story Tree/story-tree/src/app/feature/family/family-tree-page/family-tree-page.component.ts
+++ Story Tree/story-tree/src/app/feature/family/family-tree-page/family-tree-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import * as go from 'gojs';
+import { IMember } from 'src/app/core/interfaces';
+import { FamilyService } from 'src/app/core/services/family.service';
 
 @Component({
   selector: 'stapp-family-tree-page',
@@ -8,7 +10,7 @@ import * as go from 'gojs';
 })
 export class FamilyTreePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private familyService: FamilyService) { }
 
   ngOnInit(): void {
   }
@@ -18,26 +20,26 @@ export class FamilyTreePageComponent implements OnInit {
   we define property model in app.component so that both the diagram component 
   and the Inspector component can access it as @Input model
   */
-  public model: go.TreeModel = new go.TreeModel([
-    { 'key': 1, 'name': 'Stella Payne Diaz', 'title': 'CEO' },
-    { 'key': 2, 'name': 'Luke Warm', 'title': 'VP Marketing/Sales', 'parent': 1 },
-    { 'key': 3, 'name': 'Meg Meehan Hoffa', 'title': 'Sales', 'parent': 2 },
-    { 'key': 4, 'name': 'Peggy Flaming', 'title': 'VP Engineering', 'parent': 1 },
-    { 'key': 5, 'name': 'Saul Wellingood', 'title': 'Manufacturing', 'parent': 4 },
-    { 'key': 6, 'name': 'Al Ligori', 'title': 'Marketing', 'parent': 2 },
-    { 'key': 7, 'name': 'Dot Stubadd', 'title': 'Sales Rep', 'parent': 3 },
-    { 'key': 8, 'name': 'Les Ismore', 'title': 'Project Mgr', 'parent': 5 },
-    { 'key': 9, 'name': 'April Lynn Parris', 'title': 'Events Mgr', 'parent': 6 },
-    { 'key': 10, 'name': 'Xavier Breath', 'title': 'Engineering', 'parent': 4 },
-    { 'key': 11, 'name': 'Anita Hammer', 'title': 'Process', 'parent': 5 },
-    { 'key': 12, 'name': 'Billy Aiken', 'title': 'Software', 'parent': 10 },
-    { 'key': 13, 'name': 'Stan Wellback', 'title': 'Testing', 'parent': 10 },
-    { 'key': 14, 'name': 'Marge Innovera', 'title': 'Hardware', 'parent': 10 },
-    { 'key': 15, 'name': 'Evan Elpus', 'title': 'Quality', 'parent': 5 },
-    { 'key': 16, 'name': 'Lotta B. Essen', 'title': 'Sales Rep', 'parent': 3 }
-  ]);
-  //this data will be seeded later from the fb api
-  //follow the 1st workshop with theme lists example
+  //this.familyService.getMembers$().subscribe;
+  defaultMembers: IMember[];
+
+  public model: go.TreeModel = new go.TreeModel(this.defaultMembers);
+  // public model: go.TreeModel = new go.TreeModel([{ 'key': 1, 'name': 'Stella Payne Diaz', 'title': 'CEO' },
+  // { 'key': 2, 'name': 'Luke Warm', 'title': 'VP Marketing/Sales', 'parent': 1 },
+  // { 'key': 3, 'name': 'Meg Meehan Hoffa', 'title': 'Sales', 'parent': 2 },
+  // { 'key': 4, 'name': 'Peggy Flaming', 'title': 'VP Engineering', 'parent': 1 },
+  // { 'key': 5, 'name': 'Saul Wellingood', 'title': 'Manufacturing', 'parent': 4 },
+  // { 'key': 6, 'name': 'Al Ligori', 'title': 'Marketing', 'parent': 2 },
+  // { 'key': 7, 'name': 'Dot Stubadd', 'title': 'Sales Rep', 'parent': 3 },
+  // { 'key': 8, 'name': 'Les Ismore', 'title': 'Project Mgr', 'parent': 5 },
+  // { 'key': 9, 'name': 'April Lynn Parris', 'title': 'Events Mgr', 'parent': 6 },
+  // { 'key': 10, 'name': 'Xavier Breath', 'title': 'Engineering', 'parent': 4 },
+  // { 'key': 11, 'name': 'Anita Hammer', 'title': 'Process', 'parent': 5 },
+  // { 'key': 12, 'name': 'Billy Aiken', 'title': 'Software', 'parent': 10 },
+  // { 'key': 13, 'name': 'Stan Wellback', 'title': 'Testing', 'parent': 10 },
+  // { 'key': 14, 'name': 'Marge Innovera', 'title': 'Hardware', 'parent': 10 },
+  // { 'key': 15, 'name': 'Evan Elpus', 'title': 'Quality', 'parent': 5 },
+  // { 'key': 16, 'name': 'Lotta B. Essen', 'title': 'Sales Rep', 'parent': 3 }]);
 
   public setSelectedNode(node){
     this.selectedNode = node;
